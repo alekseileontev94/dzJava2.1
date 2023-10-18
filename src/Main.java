8f94455044dc11bb9d9e309735ccfe5123d2f325
Aleksei Leontev <aleksei_leontev94@mail.ru>
2023-10-18 22:48:29 +0500

    untitled1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13676;
+       int bonus = 20;
+       int miles =price / bonus;
+       System.out.println(miles + " бонусные мили");
+
+    }
+}
